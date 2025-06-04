0edbbcc02ecb5ea2b6c012bda06daf630f620dd0
MAKARD <themakard@icloud.com>
2025-06-04 12:04:42 +0100

    feat(HWM-03): Define shipments store

--- frontend/stores/shipment.store.ts
+++ frontend/stores/shipment.store.ts
@@ -1,5 +1,36 @@
 import { create } from 'zustand'
+import { immer } from 'zustand/middleware/immer'
 
-export const useShipment = create()((set) => ({
-}))
+import { GetShipmentsList } from '@/domain/apis/shipment.api';
+import { Shipment } from '@/domain/models/Shipment.model';
+import { HTTPService } from '@/infrastructure/services/HTTPService';
+
+
+const getShipmentsList = HTTPService.createRequest<GetShipmentsList.ResponseDTO, {}, {}>(GetShipmentsList.api);
+
+interface ShipmentStore {
+    shipments: Array<Shipment>;
+}
+
+export const useShipment = create<ShipmentStore>()(immer((set) => ({
+    shipments: [],
+    fetchShipmentsInWarehouse: async (warehouseId: string) => {
+        const response = await getShipmentsList({
+            body: {},
+            queryParams: {},
+            pathParams: {
+                warehouseId
+            }
+        });
+
+        set({
+            shipments: response.shipments,
+        });
+    },
+    clearShipments: () => {
+        set({
+            shipments: []
+        })
+    }
+})));
 
